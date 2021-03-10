86189690550e50bdb2d56859cb1b4bcabceee0f3
GuyVil1 <guy.vilain1@gmail.com>
2021-03-10 10:41:28 +0100

    base opérationnelle non-optimisée pour que le code reste accessible pour des débutant

---
+++ src/js/game.js
@@ -0,0 +1,242 @@
+//Variables dictionnaire
+
+let mots_5 = ["abime","acide","acier","aider","aieul","aigle","aimer","album","alger","algue","aller","amour","angle","annee","appel","apres","arabe","arbre","arche","arete","arret","assez","assis","astre","atoll","atome","autre","avant","avion","avoir","avril","bague","balai","balle","bande","barbe","barre","baton","baver","bebes","belle","betes","biere","bijou","bille","bisou","blanc","blond","boire","boite","bosse","botte","bouee","boule","bourg","bruit","buche","cache","cadre","calin","calme","canal","canne","canon","carre","carte","casse","champ","chaud","chene","chien","chier","chose","chute","clair","clown","colle","conte","corde","corps","coude","court","crabe","craie","creux","crier","croix","cruel","cuire","cygne","dames","danse","debut","degre","diner","doigt","douze","droit","drole","duree","echec","ecole","ecran","eleve","engin","envie","epais","epoux","etain","etang","etude","evier","faire","faner","farce","faute","femme","ferme","fesse","filet","fille","finir","fleur","foire","fonce","force","foret","forme","frein","frere","frite","froid","front","fruit","fumee","fumer","fusee","fusil","futur","garer","geant","geler","gener","genie","genou","glace","golfe","gomme","gorge","grain","grand","grele","guepe","gueri","habit","hache","herbe","heure","hibou","hindi","hiver","homme","hotel","huile","image","islam","jambe","jaune","jeter","jeudi","jeune","jouer","jouet","jours","lacer","lacet","laine","lampe","lapin","large","larme","laser","latin","laver","leger","lepre","lever","ligne","linge","lisse","liste","litre","livre","loupe","lourd","lundi","lutin","macon","magie","mains","maman","mardi","marie","marin","masse","matin","melon","merde","metal","metre","micro","mieux","mille","mince","mixer","moins","monde","moule","moyen","nager","nappe","navet","neige","neveu","niece","noyau","nuage","obeir","objet","ocean","odeur","œuvre","ombre","oncle","ongle","orage","ordre","outil","paire","panda","panne","passe","pates","patte","payer","peche","pelle","penis","pente","perle","peser","petit","photo","pieds","place","plage","plein","plier","pluie","plume","poche","poele","poids","poing","point","poire","pomme","pompe","poney","porte","poser","poste","pouce","poule","preau","prune","punir","puree","quand","queue","radio","radis","ramer","rampe","raper","raser","rater","rayon","reine","repas","rever","riche","roche","rouge","route","ruban","rugby","russe","sable","salir","salle","sante","sapin","savon","seize","serre","siege","signe","silex","singe","soupe","sourd","sport","stylo","sucer","sucre","suite","tabac","table","tache","taire","talon","tante","taper","tapis","tarte","tasse","tater","taupe","temps","tenir","tente","terre","teter","tigre","tirer","tissu","titre","tonne","tordu","train","trait","trier","trois","troll","trous","tuyau","unite","usine","utile","vache","vagin","vague","venir","verre","veste","veuve","vider","vieux","ville","vingt","vitre","vivre","voile","voler","volet","vulve","wagon","zebre"];
+let mots_6 = ["absent","acteur","action","adroit","adulte","agacer","agiter","agneau","alcool","amener","amuser","ananas","ancien","animal","anorak","argent","autant","auteur","autour","autrui","avaler","avance","avoine","bagage","baiser","balcon","ballon","banane","barque","barrer","bassin","batard","bateau","battre","bavoir","bercer","betail","betise","beurre","billet","bonbon","bondir","bonnet","bosser","bottes","bouche","bouder","bouger","bouton","bronze","brosse","bruler","bulles","bureau","cabane","cacher","cadeau","cahier","caisse","calcul","camion","camper","canape","canard","cancer","canyon","carnet","cartes","carton","casier","casque","casser","cecite","centre","cercle","cerise","chaine","chaise","chance","chemin","cheval","cheveu","chevre","chiant","chimie","cinema","cirque","citron","classe","cloche","cochon","coffre","cogner","colere","coller","comete","contre","copain","copier","coquet","coquin","corail","coreen","couche","coudre","couper","courir","course","cousin","couter","couver","crayon","creche","crepes","croute","cuisse","cuivre","danger","danser","debout","dedans","dehors","demain","depart","desert","dessin","dessus","devant","devoir","dindon","doigts","domino","donner","dormir","douche","dragon","droite","droits","echecs","eclair","ecorce","ecrire","effort","eglise","elever","emploi","encore","encule","endive","enerve","enfant","enorme","entier","entree","entrer","envers","epaule","eponge","epouse","equipe","espace","esprit","etaler","etoile","etroit","europe","facher","facile","famine","farine","fermer","feutre","fievre","figure","flamme","flaque","fleche","fleuve","flocon","fourmi","fraise","frange","gagner","garage","garcon","garder","gateau","gauche","gendre","geneve","gentil","geyser","girafe","glacon","gourde","gouter","goutte","graine","gramme","granit","griffe","grotte","groupe","guerir","guerre","hameau","hanche","humain","humeur","humide","hurler","imiter","intrus","isthme","jaloux","jamais","jambes","jambon","jardin","joyeux","jumeau","jungle","lacher","lagune","laisse","laiton","lancer","langue","lavabo","lecher","legume","lettre","levier","levres","lezard","louche","madame","maigre","maison","maitre","malade","mamans","manche","manege","manger","marais","marche","mariee","marier","marron","masque","menage","menton","metier","mettre","meuble","milieu","minuit","minute","modele","moitie","moment","monter","montre","moquer","morale","mordre","moteur","mouche","moufle","moulin","mourir","mousse","mouton","muguet","muscle","naitre","navire","neiger","nombre","numero","offrir","oignon","oiseau","orange","orteil","ourdou","ouvrir","paille","palais","panier","papier","paquet","pardon","pareil","parent","parfum","parler","partie","partir","passer","pattes","pedale","peigne","penser","percer","perdre","persil","phoque","photon","pierre","pigeon","pilote","pincer","piquer","piqure","plaine","plaire","plante","platre","pliage","poesie","pointe","pointu","poison","police","porter","poster","potage","poulet","poulie","poumon","poupee","poutre","prenom","preter","preuve","prince","prises","priver","propre","proton","putain","puzzle","pyjama","quartz","quatre","quinze","raisin","raison","ramper","rangee","ranger","rapide","rasoir","rateau","rayure","regard","remuer","renard","rendre","requin","rester","retard","retour","reveil","revoir","rideau","rivage","rocher","ronger","rouler","saison","salade","salive","saluer","samedi","sauter","sauver","savoir","secher","sentir","serrer","servir","sieste","sirene","soldat","soleil","solide","sombre","sommet","sonner","sortie","sortir","souple","souris","sphynx","square","suivre","taille","tampon","tendre","tennis","ticket","timbre","tiroir","tomate","tomber","tortue","traire","treize","trente","tresor","tricot","triste","trouer","tulipe","tunnel","utopie","valise","vallee","vendre","venter","ventre","verbes","verite","verser","viande","violet","virage","visage","vivant","voisin","volcan","volume","voyage","yaourt","zigzag","zombie"];
+let mots_7 = ["abeille","abricot","abriter","acheter","adresse","affaire","affiche","afrique","ajouter","algebre","aliment","alliage","allumer","ampoule","amusant","analyse","ancetre","anglais","animaux","appeler","appetit","appuyer","armoire","arreter","arriere","arriver","arroser","article","asseoir","associe","astraux","automne","avancer","bagarre","baigner","bailler","baisser","balayer","baleine","barrage","barreau","bassine","bengali","biberon","bientot","bifteck","biscuit","bizarre","blesser","blottir","boisson","boucher","bouchon","bouquet","branche","briller","buisson","cabinet","cadenas","cagoule","caillou","caliner","caprice","caresse","carotte","carreau","cederom","celibat","cerceau","cereale","cerveau","chaises","chaleur","chambre","chameau","changer","chanson","chanter","chapeau","charger","chariot","chasser","chateau","chemise","chiffon","chiffre","chimere","chinois","choisir","cholera","cigogne","ciseaux","clocher","cocotte","coffret","coiffer","collant","collier","colline","combien","comment","compose","compter","connard","content","corbeau","coucher","couette","couleur","couloir","cousine","coussin","couteau","couvert","couvrir","cracher","crapaud","cravate","creuser","crochet","croiser","croquer","cuisine","culotte","curieux","cuvette","dauphin","decorer","defiler","demolir","dernier","dessert","dessous","detroit","diamant","dinette","divorce","docteur","dossier","doucher","drapeau","ecarter","echarpe","echasse","echelle","eclater","eclipse","ecouter","ecraser","effacer","effraye","egalite","element","emmener","endroit","energie","enfiler","enlever","ennuyer","envoler","envoyer","epingle","essayer","essence","essuyer","estomac","etagere","ethique","etudier","excuser","facteur","falaise","falloir","famille","fantome","fatigue","fenetre","fermier","feuille","fevrier","ficelle","flotter","frapper","fratrie","fromage","frotter","galaxie","galette","galoper","gardien","gentile","glacier","glisser","gobelet","gonfler","goudron","gouffre","gouttes","graines","grandir","gratter","gravite","griffer","griller","grimace","grimper","gronder","guetter","habiter","hamster","haricot","hauteur","hesiter","heureux","hopital","horloge","iceberg","immense","inonder","insecte","inviter","italien","janvier","jongler","journee","juillet","justice","laisser","langues","largeur","lecture","lessive","lexique","liberte","licorne","liquide","logique","lumiere","machine","magasin","magenta","maillot","maladie","manquer","manteau","marcher","mariage","marteau","matelas","matiere","mauvais","mechant","medecin","mesurer","million","mineral","modeler","moineau","monnaie","montrer","morceau","moucher","mouette","mouille","musique","neutron","nombres","nonante","nourrir","nouveau","nuageux","obesite","obliger","occuper","oceanie","octante","octobre","optique","oreille","oublier","ouragan","ouvrier","panneau","parasol","parente","parking","passage","patient","paysage","pecheur","pedaler","peindre","peluche","pencher","pendant","pendule","penseur","percher","petrole","pinceau","piscine","placard","plafond","planche","planete","planter","plateau","pleurer","plonger","poignet","poireau","poisson","pompier","pousser","poussin","pouvoir","prairie","premier","prendre","present","presque","presser","profond","prudent","quitter","racisme","realite","recette","reciter","reculer","refuser","regaler","remplir","rentree","rentrer","reparer","repeter","reposer","reptile","reussir","revenir","riviere","robinet","ronfler","roulade","rouleau","rugueux","saigner","saisons","sardine","seconde","secouer","semaine","semelle","separer","serieux","serpent","serrure","sexisme","siffler","sifflet","silence","soigner","sommeil","sourcil","sourire","souvent","suivant","surdite","surface","tableau","tablier","tailler","tambour","tartine","tempete","terrain","terrier","theorie","torchon","tornade","toucher","tourner","tousser","trainer","tranche","travail","travaux","tremper","tricher","tromper","trousse","trouver","tsunami","univers","vampire","variole","vegetal","village","visiter","vitesse","voiture","volonte","vouloir","voyager"];
+
+//Mes variables utiles
+
+let motATrouver;//Accueille le mot à trouver
+let index;//Accueille le random qui me permet de selection l'index du dico selectioné
+let maximum;//Utile pour me MatRandom
+let nombreDeLettre;//Me permet de conserver le choix de la difficulté
+let score; //Me permet de trouver caculer le nombre d'essai
+let solution = [];//me permet de stocker les lettres trouvées par le user
+let urlImage;//Me permet d'accueillir l'url de l'image à afficher
+let gagné = false;
+var lettreTrouvée = true;
+let lettre;//Accueille la lettre entrée par le user
+let checkint;//Me permet de vérifier si l'entrée user n'est pas un chiffre
+//Les variables du dom
+let principal = document.getElementById('principal');
+
+/************************************************
+ * Les événements de selection de la difficulté *
+ ************************************************/
+let cinqLettre = document.getElementById('cinq');
+cinqLettre.addEventListener('click', () => { 
+    nombreDeLettre = 5; 
+    game();
+});
+let sixLettre = document.getElementById('six');
+sixLettre.addEventListener('click', () => { 
+    nombreDeLettre = 6; 
+    game();
+ });
+let septlettre = document.getElementById('sept');
+septlettre.addEventListener('click', () => {
+    nombreDeLettre = 7; 
+    game();
+});
+
+
+/*********************************************
+ * lancer le jeu selon la difficulté choisie *
+ *********************************************/
+
+if(nombreDeLettre == 5) {game(5)}
+else if(nombreDeLettre == 6) {game(6)}
+else if(nombreDeLettre == 7) {game(7)};
+
+/******************************
+ * Fonction principale du jeu *
+ ******************************/
+function game(){
+    //Je réinitialise mes variables
+    score = 0;
+    console.log("la difficulté est " + nombreDeLettre);
+    //Je tire au sort un mot
+    tirerAuSort(nombreDeLettre);
+    console.log(`Le mot à trouver est ${motATrouver}`);
+    //Je prépare mon tableau pour acceuillir la réponse de mon user
+    arraySolution(motATrouver);
+    console.log(solution);
+    refresh();
+}
+
+function refresh(){
+    //Je vérifie en entrant si ma variable lettre trouvée est toujours sur true
+    //dans le cas contraire, cela veut dire que le joueur à entré une mauvaise lettre 
+    //lors du tour précédent
+    if(lettreTrouvée == false){
+        score++;
+    } 
+    let variableSolution = ""; //Pour acceuillir les lettres du tableau solution
+    //Je concatène ma variable avec toutes les lettres de mon tableau solution
+    for(i=0; i<= solution.length -1; i++){
+        variableSolution += solution[i];
+    }
+    //Je lance la fonction pour rechercher l'image correspondant au score joueur
+    srcImage(score);
+    console.log(`le contenu de la variableSolution est: ${variableSolution}`);
+    //je vide la div de son contenu
+    while (principal.firstChild) principal.removeChild(principal.firstChild); 
+    //Je recode le html pour le mettre à jour avec les nouvelles données
+    principal.innerHTML = `
+    <div class='containerJeu'>
+        <div>
+            <h2>Lettres trouvées</h2>
+            <h3>${variableSolution}</h3>
+        </div>
+        <div>
+            <img src='${urlImage}' alt='url image' />
+        </div>
+    </div>`;
+
+    //Check de mon score pour voir si la partie peut continuer
+    if(score < 10){
+        setTimeout('choisirUneLettre()', 200);
+    } else {
+        alert('perduuuuuu');
+        if(confirm('Voulez vous jouer une autre partie ?'))
+        {
+            lettreTrouvée = true;
+            game() 
+        } else {
+            alert('Au revoir!');
+
+        }
+}
+}
+
+/******************************************************
+ * Pour préparer le tableau qui accueille ma solution *
+ ******************************************************/
+function arraySolution(mot){
+    solution = [];//Je réinitialise mon tableau quand ma fonction est invoquée
+    for(i=0; i<=mot.length-1; i++){
+        let tiret = "-";
+        solution.push(tiret);
+    }
+}
+
+/**************************************
+ * fonction pour tirer un mot au sort *
+ **************************************/
+
+ function tirerAuSort(difficulté){
+    if(difficulté == 5){
+        let maximum = mots_5.length;
+        index = parseInt(Math.random() * (maximum - 0) + 0);
+        motATrouver = mots_5[index];
+    } else if(difficulté == 6) {
+        let maximum = mots_6.length;
+        index = parseInt(Math.random() * (maximum - 0) + 0);
+        motATrouver = mots_6[index];
+    } else if(difficulté == 7){
+        let maximum = mots_7.length;
+        index = parseInt(Math.random() * (maximum - 0) + 0);
+        motATrouver = mots_7[index];
+    }
+}
+
+/**************************************
+ * fonction pour définir l'url image  *
+ **************************************/
+
+ function srcImage(score){
+    switch(score){
+        case 0:
+            urlImage = './src/img/pendu000.png';
+            break;
+        case 1:
+            urlImage = './src/img/pendu001.png';
+            break;
+        case 2:
+            urlImage = './src/img/pendu002.png';
+            break; 
+        case 3:
+            urlImage = './src/img/pendu003.png';
+            break;
+        case 4:
+            urlImage = './src/img/pendu004.png';
+            break;
+        case 5:
+            urlImage = './src/img/pendu005.png';
+            break;
+        case 6:
+            urlImage = './src/img/pendu006.png';
+            break;
+        case 7:
+            urlImage = './src/img/pendu007.png';
+            break;
+        case 8:
+            urlImage = './src/img/pendu008.png';
+            break;
+        case 9:
+            urlImage = './src/img/pendu009.png';
+            break;
+        case 10:
+            urlImage = './src/img/pendu010.png';
+            break;
+
+    }
+}
+/**************************************************
+* Permettre à l'utilisateur de choisir une lettre *
+***************************************************/
+
+function choisirUneLettre(){
+    do{
+        lettre = "";
+        lettre = prompt('choisissez une lettre');
+        console.log(lettre);
+    } while (lettre == " " || lettre == null || lettre == "");
+    console.log('je sors de ma boucle');
+    
+    //Je lance ma fonction de comparaison
+    verifierEntrée(lettre);
+}
+
+/**************************************
+ * Véridier lettre entrée par le user *
+ **************************************/
+
+function verifierEntrée(entreeUser){
+    lettreTrouvée = false;
+    for(i = 0; i <= motATrouver.length -1; i++){
+        if(entreeUser == motATrouver[i]){
+            solution[i] = lettre;
+            lettreTrouvée = true;
+            console.log(lettreTrouvée);
+        }
+    }
+    console.log(lettreTrouvée);
+    vérifierVictoire();
+}
+
+/***********************************
+ * Véridier si le mot à été trouvé *
+ ***********************************/
+function vérifierVictoire(){
+    let pointVictoire = 0;
+    for(i = 0; i <= motATrouver.length -1; i++){
+        if(solution[i] == motATrouver[i]){
+            pointVictoire++
+        }
+    }
+
+    if(pointVictoire == motATrouver.length){
+        alert(`Bravo, le mot à trouver était bien ${motATrouver}, tu as gagnés cette manche`);
+        if(confirm('Voulez vous jouer une autre partie ?'))
+        {
+            lettreTrouvée = true;
+            game() 
+        } else {
+            alert('Au revoir!');
+
+        }
+    } else {
+        refresh();
+    }
+}
